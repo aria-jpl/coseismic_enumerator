583d9d0e0ca27486eb1e67f814d6da6b3fadbd74
Al Niessner <Al.Niessner@xxx.xxx>
2021-04-23 13:02:26 -0700

    change from location to water masked location
    Put a commonly usable function to extract the appropriate location from a geolocated object. If it finds the water masked location it will use that but will fall back to the general location when not present.
    
    All uses of [location] are changed to get_location i active.py and footprint.puy

--- constants.py
+++ constants.py
@@ -15,3 +15,11 @@ class NoOrbitsAvailable(Exception):
 class NotEnoughHistoricalData(Exception):
     '''Isolate an exception for when cannot find historical data for an AOI'''
     pass
+
+def get_location (aoi:{}):
+    if ('metadata' in aoi and 'event_metadata' in aoi['metadata'] and
+        'water_masked_geojson_polygon' in aoi['metadata']['event_metadata']):
+        print ('->     found water masked location')
+        loc = aoi['metadata']['event_metadata']['water_masked_geojson_polygon']
+    else: loc = aoi['location']
+    return loc
